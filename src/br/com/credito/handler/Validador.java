24515822dea5a9bf138a2a8f49adb44beaa81dd3
João Morais <jv.timotio@gmail.com>
2026-02-28 01:20:34 -0300

    Main and Validador

--- src/br/com/credito/handler/Validador.java
+++ src/br/com/credito/handler/Validador.java
@@ -1,4 +1,21 @@
 package br.com.credito.handler;
 
-public class Validador {
+import br.com.credito.model.Cliente;
+
+public abstract class Validador {
+
+    protected Validador proximo;
+
+    public void setProximo(Validador proximo){
+        this.proximo = proximo;
+    }
+
+    public abstract boolean validar(Cliente cliente);
+
+    protected boolean proximo(Cliente cliente) {
+        if (proximo == null) {
+            return true;
+        }
+        return proximo.validar(cliente);
+    }
 }
--- src/br/com/credito/model/Cliente.java
+++ src/br/com/credito/model/Cliente.java
@@ -1,4 +1,21 @@
 package br.com.credito.model;
 
 public class Cliente {
+
+    private String nome;
+    private double renda;
+    private int score;
+    private boolean possuiRestricao;
+
+    public Cliente(String nome, double renda, int score, boolean possuiRestricao){
+        this.nome = nome;
+        this.renda = renda;
+        this.score = score;
+        this.possuiRestricao = possuiRestricao;
+    }
+
+    public String getNome(){return nome;}
+    public double getRenda () {return renda;}
+    public int getScore (){return score;}
+    public boolean isPossuiRestricao (){return possuiRestricao;}
 }
